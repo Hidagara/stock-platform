9df4eb88d3c2cbbab710aa7beaab0d4f600c415f
hidagara <hidagara@gmail.com>
2018-01-12 05:54:05 +0300

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Торгуйте акциями или просмотрите свой портфель</h1>
+        <h6>Вы можете сохранить ваши данные</h6>
+        <h6>Нажмите "Закончить день" для начала нового дня</h6>
+        <hr>
+        <p>Ваши средства: {{ funds | currency }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            funds() {
+                return this.$store.getters.funds;
+            }
+        }
+    }
+</script>
